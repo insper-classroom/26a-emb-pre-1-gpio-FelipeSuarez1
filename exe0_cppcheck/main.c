34065a6c13288b42394c590a070a22bfa2e7c8e3
Felipe Suarez <felipefcs@al.insper.edu.br>
2026-02-20 08:54:51 -0300

    Segunda tentativa ex0

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -3,15 +3,15 @@
 
 #define NUM_ELEMENTS 5
 
-int multiplyNumbers(int *x, int y) {
-    int product = *x * y;
+int multiplyNumbers(int x, int y) {
+    int product = x * y;
     return product;
 }
 
 int main(void) {
     int a = 5;
     int b = 5;
-    int result = multiplyNumbers(&a, b);
+    int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
     for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
